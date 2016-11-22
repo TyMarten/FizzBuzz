5299bbe5efa6d93f8c8b1137688ebd91f1369546
TyMarten <Tyamarten@gmail.com>
2016-11-22 17:27:44 -0600

    FizzBuzz
    A4, Q3

---
+++ src/FizzBuzz.java
@@ -0,0 +1,39 @@
+/**
+ * 
+ */
+
+/**
+ * @author t.marten
+ *
+ */
+public class FizzBuzz {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+	for(int i = 1; i <= 100; i++){
+		
+		if(i%15 == 0){
+		System.out.println("FizzBuzz");
+			
+		}
+		else if (i%3 == 0){ 
+		System.out.println("Fizz");
+		
+		}
+		else if (i%5 == 0){
+		System.out.println("Buzz");
+			
+		}
+		
+		else{
+			System.out.println("i");
+			
+			
+			}
+		}
+	}
+}
